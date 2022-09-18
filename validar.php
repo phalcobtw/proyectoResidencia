ebda459948af488b7477e651ebcc01327637ecc9
phalcobtw <laxz_16@hotmail.com>
2022-09-18 14:59:49 -0600

    validacion de sesion en login de index

---
+++ validar.php
@@ -0,0 +1,24 @@
+<?php
+
+$usuario = $_POST['usuario'];
+$contra = $_POST['contraseña'];
+
+session_start();
+$_SESSION['usuario'] = $usuario;
+include "bd_conn.php";
+$consulta = "SELECT * FROM `usuarios` WHERE nombreusuario = '$usuario' AND password = '$contra'";
+
+$resultado = mysqli_query($conn, $consulta);
+$filas = mysqli_num_rows($resultado);
+
+if ($filas) {
+    header("Location:seleccionModulos.html");
+}
+else{
+    header("Location:index.php?msg=Datos incorrectos");
+}
+mysqli_free_result($resultado);
+mysqli_close($conn);
+
+
+?>

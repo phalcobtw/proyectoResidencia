df3651e2d449672f749f71ccc017992fc205dd27
phalcobtw <laxz_16@hotmail.com>
2022-09-18 15:46:42 -0600

    validarSesion2 para paginas dentro de carpetas

---
+++ validarSesion2.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+$varsession = $_SESSION['usuario'];
+
+if ($varsession == null || $varsession = '') {
+    header("location:../index.php?msg=Acceso no autorizado, por favor, inicie sesión.");
+}
+
+?>

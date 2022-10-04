e486d5b064ebf4f18d64258449bc03e908402ef3
phalcobtw <laxz_16@hotmail.com>
2022-10-03 19:29:46 -0600

    fecha por zona horaria en variable

--- bd_conn.php
+++ bd_conn.php
@@ -3,6 +3,9 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "siatec";
+date_default_timezone_set('America/Mazatlan');
+$date = date('Y/m');
+$fulldate = date('Y/m/d');
 
 $conn = mysqli_connect($servername,$username,$password,$dbname);
 

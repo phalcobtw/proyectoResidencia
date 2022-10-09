e6974796eae6475c6a6b766451d86e532079a0fc
Kevin Arnista <denjinpunch@gmail.com>
2022-10-08 19:33:37 -0600

    cambio siscon a php

---
+++ modConta/siscon.php
@@ -0,0 +1,69 @@
+<?php
+include "../validarSesion2.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SISCOM- SIATEC</title>
+    <link rel="icon" type="image/png" sizes="16x16" href="../img/favicon.png">
+    <link rel="stylesheet" href="../css/styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+    <header class="p-3 bg-primary text-white">
+        <div class="divheader">
+        <div class="divlogo">
+            <img src="../img/logosep.png" alt="logosep" class="logosep">
+            <b class="titulo">SISCOM</b>
+        </div>
+        </div>
+    </header>
+                         <!-- todo el menu -->
+                         <ul class="menu">
+                            <li>Catalogos      
+                                <ul class="dropdownmenu">
+                                <li><a href="" class="menulinks">Personal</a></li>
+                               </ul>
+                            </li>
+                            <li>Movimientos      
+                                <ul class="dropdownmenu">
+                                <li><a href="" class="menulinks">Anticipos</a></li>
+                                <li><a href="" class="menulinks">Comprobaciones</a></li>
+                                <li><a href="" class="menulinks">Verificar</a></li>
+                               </ul>
+                            </li>
+                            <li>Traspaso
+                                <ul class="dropdownmenu">
+                                <li><a href="" class="menulinks">Verificaci&oacute; de Informaci&oacute;n</a></li>
+                                <li><a href="" class="menulinks">Traspaso a Contabilidad</a></li>
+                                <li><a href="" class="menulinks">Reporte del Corto</a></li>
+                               </ul>
+                            </li>
+                            <li>Reportes
+                                <ul class="dropdownmenu">
+                                <li><a href="" class="menulinks">Generales</a></li>
+                                <li><a href="" class="menulinks">Anticipos</a></li>
+                                <li><a href="" class="menulinks">Fuente de Ingresos</a></li>
+                                <li><a href="" class="menulinks">Partidas</a></li>
+                               </ul>
+                            </li>
+                            <li>Utilerias        
+                                <ul class="dropdownmenu">
+                                <li><a href="" class="menulinks">Cambio de Mes</a></li>
+                                <li><a href="" class="menulinks">Indexar Archivos</a></li>
+                                <li><a href="" class="menulinks">Control de Usuarios</a></li>
+                                <li><a href="" class="menulinks">Configurar Impresora</a></li>
+                                <li><a href="" class="menulinks">Par&aacute;metros</a></li>
+                                <li><a href="" class="menulinks">Cierre de Ejercicio</a></li>
+                                <li><a href="" class="menulinks">Configurar</a></li>
+                               </ul>
+                            </li>
+                            <li><a href="../seleccionModulos.html" class="menulinks">Salir</a></li>
+                        </ul>
+                        <!-- aqui termina -->
+</body>
+</html>

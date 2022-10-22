204999f11cf09734309cef43ae398c71104a595a
@denjinpunch <denjinpunch@gmail.com>
2022-10-21 18:27:06 -0600

    mas actualizaciones de navbar

--- modBancos/editarbanks.php
+++ modBancos/editarbanks.php
@@ -22,76 +22,95 @@ if (isset($_POST['submit'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CONTRED- SIATEC</title>
+    <title>SISBAN- SIATEC</title>
     <link rel="icon" type="image/png" sizes="16x16" href="../img/favicon.png">
     <link rel="stylesheet" href="../css/styles.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-</head>
-
+    <link rel="stylesheet" href="../css/navbar.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"crossorigin="anonymous"></script>  
+  </head>
 <body>
     <header class="p-3 bg-primary text-white">
         <div class="divheader">
-            <div class="divlogo">
-                <img src="../img/logosep.png" alt="logosep" class="logosep">
-                <b class="titulo">SISBAN</b>
-            </div>
+        <div class="divlogo">
+            <img src="../img/logosep.png" alt="logosep" class="logosep">
         </div>
-        <!-- todo el menu -->
+        </div>
     </header>
-    <ul class="menu">
-        <li>Cheques
-            <ul class="dropdownmenu">
-                <li><a href="" class="menulinks">Emisi&oacute;n de Cheques en Espera</a></li>
-                <li><a href="" class="menulinks"> Emisi&oacute;n de Cheques Especiales</a></li>
-                <li><a href="" class="menulinks"> Cancelaciones </a>
-                <ul class="dropdownmenu">                                
-                                <li><a href="cancelacionDeCheques.php" class="menulinks"> Cancelacion de Cheques</a></li>
-                               <li><a href="cancelacionChequesMeses.php" class="menulinks"> Cancelacion de Cheques de Meses Anteriores </a></li>
-                           </ul>
-            </li>
-            </ul>
-        </li>
-        <li>Depositos
-            <ul class="dropdownmenu">
-                <li><a href="depositos.php" class="menulinks">Dep&oacute;sitos Bancarios </a></li>
-            </ul>
-        </li>
-        <li>Traspasos
-            <ul class="dropdownmenu">
-                <li><a href="traspasos.php" class="menulinks">Traspasos entre Cuentas</a></li>
-            </ul>
-        </li>
-        <li>Catalogos
-            <ul class="dropdownmenu">
-            <li><a href="catalogobanks.php" class="menulinks">Bancos</a></li>
-                <li><a href="catalogoCuentas.php" class="menulinks">Cuentas</a></li>
-            </ul>
-        </li>
-        <li>Reportes
-            <ul class="dropdownmenu">
-                <li><a href="" class="menulinks">Depositos</a></li>
-                <li><a href="" class="menulinks">Cheques</a></li>
-                <li><a href="" class="menulinks">Saldos</a></li>
-                <li><a href="" class="menulinks">Prueba de Impresor</a></li>
-            </ul>
-        </li>
-        <li>Utilerias
-            <ul class="dropdownmenu">
-                <li><a href="" class="menulinks">Cambio de Mes</a></li>
-                <li><a href="" class="menulinks">Cierre de Ejercicio</a></li>
-                <li><a href="" class="menulinks">Indexar Archivos</a></li>
-                <li><a href="" class="menulinks">Reconstruir Saldos</a></li>
-                <li><a href="../utilerias/controlUsuarios.php" class="menulinks">Control de Usuarios</a></li>
-                                <li><a href="" class="menulinks">Configurar Impresora</a></li>
-                                <li><a href="" class="menulinks">Par&aacute;metros Sistema</a></li>
-                                <li><a href="" class="menulinks">Par&aacute;metros Poliza-Cheque</a></li>
-                                <li><a href="../utilerias/configITLM.php" class="menulinks">Configurar</a></li>
-            </ul>
-        </li>
-        <li><a href="../seleccionModulos.php" class="menulinks">Salir</a></li>
-    </ul>
+                         <!-- todo el menu -->
+                         <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+                          <div class="container-fluid">
+                          <a class="navbar-brand" href="#">SISBAN</a>
+                          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main_nav"  aria-expanded="false" aria-label="Toggle navigation">
+                          <span class="navbar-toggler-icon"></span>
+                          </button>
+                          <div class="collapse navbar-collapse justify-content-md-center" id="main_nav">
+                            <ul class="navbar-nav">
+                              <li class="nav-item dropdown" id="dropdown"> 
+                                <a class="nav-link dropdown-toggle active" href="#" data-bs-toggle="dropdown">Cheques</a>
+                                <ul class="dropdown-menu">
+                                  <li> <a class="dropdown-item" href="#">Emisi&oacute;n de Cheques en Espera</a></li>
+                                  <li> <a class="dropdown-item" href="emisionChequesEspecial.php">Emisi&oacute;n de Cheques Especiales</a></li>
+                                  <li> <a class="dropdown-item" href="#">Cancelaciones &raquo; </a>
+                                    <ul class="submenu dropdown-menu">
+                                      <li><a class="dropdown-item" href="cancelacionDeCheques.php">Cancelacion de Cheques</a></li>
+                                      <li><a class="dropdown-item" href="cancelacionChequesMeses.php">Cancelacion de Cheques de Meses Anteriores</a></li>
+                                    </ul>
+                                  </li>
+                                </ul>
+                              </li>
+                              <li class="nav-item dropdown" id="dropdown">
+                                <a class="nav-link dropdown-toggle active" href="#" data-bs-toggle="dropdown"> Depositos</a>
+                                <ul class="dropdown-menu">
+                                  <li> <a class="dropdown-item" href="depositos.php">Dep&oacute;sitos Bancarios</a></li>
+                                </ul>
+                              </li>
+                              <li class="nav-item dropdown" id="myDropdown">
+                                <a class="nav-link dropdown-toggle active" href="#" data-bs-toggle="dropdown">Traspasos</a>
+                                <ul class="dropdown-menu">
+                                  <li> <a class="dropdown-item" href="traspasos.php">Traspasos entre Cuentas</a></li>
+                                </ul>
+                              </li>
+                              <li class="nav-item dropdown" id="dropdown">
+                                  <a class="nav-link dropdown-toggle active" href="#" data-bs-toggle="dropdown">Catalogos</a>
+                                  <ul class="dropdown-menu">
+                                    <li> <a class="dropdown-item" href="catalogobanks.php">Bancos</a></li>
+                                    <li> <a class="dropdown-item" href="catalogoCuentas.php">Cuentas</a></li>
+                                  </ul>
+                                </li>
+                                <li class="nav-item dropdown" id="dropdown">
+                                    <a class="nav-link dropdown-toggle active" href="#" data-bs-toggle="dropdown">Reportes</a>
+                                    <ul class="dropdown-menu">
+                                      <li> <a class="dropdown-item" href="#">Depositos</a></li>
+                                      <li> <a class="dropdown-item" href="#">Cheques</a></li>
+                                      <li> <a class="dropdown-item" href="#">Saldos</a></li>
+                                      <li> <a class="dropdown-item" href="#">Prueba de Impresor</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item dropdown" id="dropdown">
+                                      <a class="nav-link dropdown-toggle active" href="#" data-bs-toggle="dropdown">Utilerias</a>
+                                      <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="#">Cambio de Mes</a></li>
+                                        <li><a class="dropdown-item" href="#">Cierre de Ejercicio</a></li>
+                                        <li><a class="dropdown-item" href="#">Indexar Archivos</a></li>
+                                        <li><a class="dropdown-item" href="#">Reconstruir Saldos</a></li>
+                                        <li><a class="dropdown-item" href="../utilerias/controlUsuarios.php">Control de Usuarios</a></li>
+                                        <li><a class="dropdown-item" href="#">Configurar Impresora</a></li>
+                                        <li><a class="dropdown-item" href="#">Par&aacute;metros Sistema</a></li>
+                                        <li><a class="dropdown-item" href="#">Par&aacute;metros Poliza-Cheque</a></li>
+                                        <li><a class="dropdown-item" href="../utilerias/configITLM.php">Configurar</a></li>                                      
+                                </ul>
+                              </li>
+                              <li class="nav-item">
+                                <a class="nav-link active" href="../seleccionModulos.php" >Salir</a>
+                              </li>
+                            </ul>
+                          </div>
+                          <!-- navbar-collapse.// -->
+                          </div>
+                          <!-- container-fluid.// -->
+                          </nav>
+                        <!-- aqui termina -->
     <div class="container" style="margin-bottom: 35px;">
         <div class="text-center mb-4 fs-4">Editar Banco</div>
         <?php

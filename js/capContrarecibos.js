3f575e6820b7f573ce7b34270b51ec85d501ad14
phalcobtw <laxz_16@hotmail.com>
2022-10-24 19:29:04 -0600

    funcion sacar datos del row de la tabla programa

--- js/capContrarecibos.js
+++ js/capContrarecibos.js
@@ -4,4 +4,16 @@ listaSelect.addEventListener("change",function () {
     var elemSeleccionado = listaSelect.value;
     var textF = document.getElementById('txtCuenta');
     textF.value = elemSeleccionado;
+
+});
+
+
+//saco valores de tabla programa al hacer click en boton agregar
+$(".progbutton").click(function() {
+    var $rowprogra = $(this).closest("tr");    // Find the row
+    var $claveprog = $rowprogra.find(".progtd").text(); // Find the text
+    var $descrprog = $rowprogra.find(".progdesctd").text();
+    document.getElementById("claveprog").textContent = $claveprog;
+    document.getElementById("descprog").textContent = $descrprog;
+    /* alert($claveprog); */
 });

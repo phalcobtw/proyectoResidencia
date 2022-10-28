012d3f3aa0a2416fe6e20711911810b1b8cb9e91
phalcobtw <laxz_16@hotmail.com>
2022-10-27 18:56:26 -0600

    script limpia y toma datos de tables

--- js/capContrarecibos.js
+++ js/capContrarecibos.js
@@ -23,7 +23,7 @@ $(".cuentasbutton").click(function() {
     if (valor === "2101" && $tiporetcuenta === "HON") {
         $("#tableIVA").show();
         $("#tableISR").show();
-        alert("work");
+        /* alert("work"); */
     }
 else{
     $("#tableIVA").hide();
@@ -31,3 +31,53 @@ $("#tableISR").hide();
 }
 });});
 
+$(".actbutton").click(function () {
+    var $rowact = $(this).closest("tr");    // Find the row
+    var $claveact = $rowact.find(".acttd").text(); // Find the text
+    var $descact = $rowact.find(".descacttd").text();    
+    document.getElementById("claveact").textContent = $claveact;
+    document.getElementById("descact").textContent = $descact;
+});
+
+$(".partbutton").click(function () {
+    var $rowpart = $(this).closest("tr");    // Find the row
+    var $clavepart = $rowpart.find(".parttd").text(); // Find the text
+    var $descpart = $rowpart.find(".descparttd").text();    
+    document.getElementById("clavepart").textContent = $clavepart;
+    document.getElementById("descpart").textContent = $descpart;
+});
+
+$(".depbutton").click(function () {
+    var $rowdep = $(this).closest("tr");    // Find the row
+    var $clavedep = $rowdep.find(".deptd").text(); // Find the text
+    var $descdep = $rowdep.find(".descdeptd").text();    
+    document.getElementById("clavedep").textContent = $clavedep;
+    document.getElementById("descdep").textContent = $descdep;
+});
+
+
+$(".limpiarCuentas").click(function (){
+    $("#clavecuenta").text("");
+    $("#descrcuenta").text("");
+    $("#tiporetcuenta").text("");
+});
+
+$(".limpiarPrograma").click(function (){
+    $("#claveprog").text("");
+    $("#descprog").text("");
+});
+
+$(".limpiarActividad").click(function (){
+    $("#claveact").text("");
+    $("#descact").text("");
+});
+
+$(".limpiarPartida").click(function (){
+    $("#clavepart").text("");
+    $("#descpart").text("");
+});
+
+$(".limpiarDepartamento").click(function (){
+    $("#clavedep").text("");
+    $("#descdep").text("");
+});

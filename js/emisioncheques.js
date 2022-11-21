3da03e4018b66225dc14e88f41dda44cc2b6475e
phalcobtw <laxz_16@hotmail.com>
2022-11-21 15:56:48 -0700

    script escoger data de table por medio de button

--- js/emisioncheques.js
+++ js/emisioncheques.js
@@ -1,3 +1,17 @@
+$(".emicheqButton").click(function () {
+  var $rowcheque = $(this).closest("tr");    // Find the row
+  var $cuenta = $rowcheque.find(".cuencartd").text(); // Find the text
+  var $banco = $rowcheque.find(".cuencardesctd").text();
+  var $clave = $rowcheque.find(".cuencardesctd").text();
+  var $saldo = $rowcheque.find(".cuencardesctd").text();
+  document.getElementById("claveInput").value = $clave;
+  document.getElementById("cuentaInput").value = $cuenta;
+  document.getElementById("bancoInput").value = $banco;
+  document.getElementById("saldoInput").value = $saldo;
+});
+
+
+
 var myModal = document.getElementById('myModal')
 var myInput = document.getElementById('myInput')
 myModal.addEventListener('shown.bs.modal', function () {

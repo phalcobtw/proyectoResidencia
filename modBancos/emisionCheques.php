d56fe645726f6109e0e160160417a9cd47478db0
@denjinpunch <denjinpunch@gmail.com>
2022-11-21 14:39:11 -0700

    estructura cheques emisionCheques

--- modBancos/emisionCheques.php
+++ modBancos/emisionCheques.php
@@ -16,6 +16,7 @@ include "../bd_conn.php";
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 </head>
 
 <body>
@@ -138,7 +139,9 @@ include "../bd_conn.php";
                         <td>$' . $row["importe"] . '</td>
                         <td>' . $row["fuenteingresos"] . '</td>
                         <td>' . $row["estadopoliza"] . '</td>
-                        <td><a href=".php?id=' . $row["id"] . '" class="link-dark"><i class="fa-solid fa-pen-to-square fs-5 me-3"></i></a></td>                        
+                        <td><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        <i class="fa-solid fa-pen-to-square fs-5 me-3"></i>
+                        </button></td>                        
                     </tr>
                     ';
                 }
@@ -146,6 +149,76 @@ include "../bd_conn.php";
             </tbody>
         </table>
   </div>
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <div class="row">
+                                    <div class="table-wrapper">
+                                        <table class="table table-sm table-hover table-striped palco-table">
+                                            <thead class="table-dark">
+                                                <th>
+                                                    Cuenta de Cargo
+                                                </th>
+                                                <th>
+                                                    Descripcion
+                                                </th>
+                                                <th>
+                                                    Seleccionar
+                                                </th>
+                                            </thead>
+                                            <tbody class="palco-tbody">
+                                                <?php
+                                                $sqlcar = "SELECT * from `cuentacatalogo` WHERE LEFT(cuenta,4) BETWEEN 6100 AND 6900";
+                                                $resultcar = mysqli_query($conn, $sqlcar);
+                                                while ($rowcargo = mysqli_fetch_array($resultcar)) {
+                                                    echo '
+                                <tr>
+                                    <td class="cuencartd">' . $rowcargo["cuenta"] . '</td>
+                                    <td class="cuencardesctd">' . $rowcargo["descripcion"] . '</td>
+                                    <td><button type="button" class="btn btn-outline-dark cuencarbutton">Agregar</button></td> 
+                                </tr>
+                                ';
+                                                }
+                                                echo '
+                            
+                            '
+                                                ?>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    <div class="row text-center">
+                                    <label for="" class="form-label label-txt">Datos Bancos</label>
+                    <div class="col-6 mt-2">
+                        <label class="form-label">Clave:</label>
+                        <input type="text" class="form-control" name="fuenteing" readonly>
+                    </div>
+                    <div class="col-6 mt-2">
+                        <label class="form-label">Cuenta:</label>
+                        <input type="text" class="form-control" name="poliza" readonly>
+                    </div>
+                    <div class="col-6 mt-2">
+                        <label class="form-label">Banco:</label>
+                        <input type="text" class="form-control" name="fuenteing" readonly>
+                    </div>
+                    <div class="col-6 mt-2">
+                        <label class="form-label">Saldo:</label>
+                        <input type="text" class="form-control" name="poliza" readonly>
+                    </div>
+                </div>
+                                    </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+        <button type="button" class="btn btn-primary">Actualizar</button>
+      </div>
+    </div>
+  </div>
+</div>
 </body>
-
+<script src="../js/capContrarecibos.js"></script>
 </html>
